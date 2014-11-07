558aa064aa21dd0fd9307e2368906c45275a99dd
Nicholas Long <nicholas.long@nrel.gov>
2014-11-06 17:24:33 -0700

    update spreadsheet format to include worker inits

---
+++ worker_scripts/example_init_file.rb
@@ -0,0 +1,26 @@
+# This is an Example worker initilation file. The file has to follow general Ruby conventions.
+#   File name must for the snake case (underscore case) of the class name. For example: WorkerInit = worker_init
+#   
+
+class ExampleInitFile
+
+  # Not required but can be used to initialize variables. No arguments can be passed to this method
+  def initialize
+    # do nothing in this example
+  end
+
+  # The run method is where all the work will be performed. This can have a variable length of arguments, which have
+  # to match the cell from the Excel spreadsheet
+  def run(arg_1, *args)
+    puts args
+    new_value = arg_1
+
+    args
+  end
+
+  # this is optional. If it exists, the method will be called at the end. No arguments will be passed to this method.
+  def finalize
+    # do nothing
+  end
+end
+
